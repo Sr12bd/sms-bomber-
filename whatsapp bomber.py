0798ae89b23708a2d58a34ac30f77379f9e8916f
Sr12bd <115818842+Sr12bd@users.noreply.github.com>
2022-11-09 16:07:56 +0200

    Add files via upload

---
+++ whatsapp bomber.py
@@ -0,0 +1,68 @@
+import pyautogui as gui
+import time
+import pyfiglet 
+import colorama
+import time
+import os
+from colorama import*
+text=pyfiglet.figlet_format("sms bomber ") 
+print(Fore.GREEN+text)
+print(Fore.GREEN+"                      BY SR12BD")
+print("******************************************")
+time.sleep(0.5)
+print("connecting")
+time.sleep(3)
+print("open whatsapp, click on the contact you want to bomb")
+time.sleep(2)
+print("============================================")
+print("")
+
+num=input("Number of messages")
+mes=input("Enter your message")
+time.sleep(2)
+for i in range (int(num)):
+    gui.typewrite(mes)
+    gui.press('enter')
+
+emu=input("type X to update to latest verstion")
+if emu!= "x":
+    import pyautogui as gui
+import time
+import pyfiglet 
+import colorama
+import time
+import os
+from colorama import*
+text=pyfiglet.figlet_format("sms bomber ") 
+print(Fore.GREEN+text)
+print(Fore.GREEN+"                      BY SR12BD")
+print("******************************************")
+time.sleep(0.5)
+print("connecting")
+time.sleep(3)
+print("open whatsapp, click on the contact you want to bomb")
+time.sleep(2)
+print("============================================")
+print("")
+
+num=input("Number of messages")
+mes=input("Enter your message")
+time.sleep(2)
+for i in range (int(num)):
+    gui.typewrite(mes)
+    gui.press('enter')
+if emu == "X":
+  print(Fore.GREEN + "Updating Ip Tracker")
+os.system("""
+    cd
+    rm -f -r Ip-Tracker
+    git clone https://github.com/spider863644/Ip-Tracker
+            
+    """)
+            
+print(Fore.BLUE + """Now type the following command
+            cd $HOME
+            cd Ip-Tracker
+            python3 tracker.py
+            """)
+exit()
