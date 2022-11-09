cb2a6d08bd560f5924aadac61ec1d0168fcdb71d
Sr12bd <115818842+Sr12bd@users.noreply.github.com>
2022-11-09 16:25:47 +0200

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -3,66 +3,26 @@ import time
 import pyfiglet 
 import colorama
 import time
-import os
 from colorama import*
 text=pyfiglet.figlet_format("sms bomber ") 
 print(Fore.GREEN+text)
 print(Fore.GREEN+"                      BY SR12BD")
+cont=input("press enter to continue:")
 print("******************************************")
 time.sleep(0.5)
-print("connecting")
+print("connecting...........")
 time.sleep(3)
 print("open whatsapp, click on the contact you want to bomb")
 time.sleep(2)
 print("============================================")
 print("")
-
-num=input("Number of messages")
-mes=input("Enter your message")
+cont=input("press enter to continue:")
+print("connecting once more...........")
+time.sleep(4)
+print("=============================================")
+num=input(Fore.BLUE+"[+] Number of messages")
+mes=input(Fore.BLUE+"[+] Enter your message")
 time.sleep(2)
 for i in range (int(num)):
     gui.typewrite(mes)
     gui.press('enter')
-
-emu=input("type X to update to latest verstion")
-if emu!= "x":
-    import pyautogui as gui
-import time
-import pyfiglet 
-import colorama
-import time
-import os
-from colorama import*
-text=pyfiglet.figlet_format("sms bomber ") 
-print(Fore.GREEN+text)
-print(Fore.GREEN+"                      BY SR12BD")
-print("******************************************")
-time.sleep(0.5)
-print("connecting")
-time.sleep(3)
-print("open whatsapp, click on the contact you want to bomb")
-time.sleep(2)
-print("============================================")
-print("")
-
-num=input("Number of messages")
-mes=input("Enter your message")
-time.sleep(2)
-for i in range (int(num)):
-    gui.typewrite(mes)
-    gui.press('enter')
-if emu == "X":
-  print(Fore.GREEN + "Updating your BOMBER")
-os.system("""
-    cd
-    rm -f -r Ip-Tracker
-    git clone https://github.com/Sr12bd/sms-bomber-.git
-            
-    """)
-            
-print(Fore.BLUE + """Now type the following command
-            cd $HOME
-            cd sms bomber
-            python3 bomb.py
-            """)
-exit()
